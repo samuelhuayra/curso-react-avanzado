ec8f50880f9ae00489093b311e2281661a7abe52
Samuel Huayra <samuelhuayra@gmail.com>
2020-04-22 01:48:16 -0400

    producto terminado

---
+++ crmcliente/pages/_app.js
@@ -0,0 +1,12 @@
+import { ApolloProvider } from "@apollo/client";
+import client from "../config/apollo";
+
+const MyApp = ({Component,pageProps})=>{
+    console.log('Desde _app.js');
+    return(
+        <ApolloProvider client={client}>
+            <Component {...pageProps}/>
+        </ApolloProvider>
+    )
+}
+export default MyApp;
--- proyecto terminado/crmcliente/pages/nuevacuenta.js
+++ proyecto terminado/crmcliente/pages/nuevacuenta.js
@@ -1,82 +1,125 @@
-import React,{useState} from 'react';
-import Layout from '../components/layout'
-import { useFormik } from "formik";
-import * as Yup from 'yup'
-import { useMutation, gql } from "@apollo/client";
+import React, {useState} from 'react';
+import { useRouter } from 'next/router';
+import Layout from '../components/Layout';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useMutation, gql } from '@apollo/client';
 
 const NUEVA_CUENTA = gql`
-    mutation nuevoUsuario($input:UsuarioInput){
-        nuevoUsuario(input:$input){
+    mutation nuevoUsuario($input: UsuarioInput) {
+        nuevoUsuario(input: $input) {
             id
             nombre
             apellido
             email
         }
     }
-`
+`;
 
-export default function NuevaCuenta() {
 
-    //State para el mensaje
-    const [mensaje,guardarMensaje] = useState(null)
-    
-    const [nuevoUsuario] = useMutation(NUEVA_CUENTA)
+const NuevaCuenta = () => {
 
-    //Validacion del formulario
+    // State para el mensaje
+    const [mensaje, guardarMensaje] = useState(null)
+
+    // Mutation para crear nuevos usuarios
+    const [ nuevoUsuario ] = useMutation(NUEVA_CUENTA);
+
+    // Routing
+    const router = useRouter();
+
+
+
+    // Validación del formulario
     const formik = useFormik({
         initialValues: {
             nombre: '',
             apellido: '',
             email: '',
-            password: '',
-        },
+            password: ''
+        }, 
         validationSchema: Yup.object({
             nombre: Yup.string()
-                .required('El Nombre es Obligatorio'),
+                        .required('El Nombre es Obligatorio'), 
             apellido: Yup.string()
-                .required('El Apellido es obligatorio'),
+                        .required('El Apellido es obligatorio'),
             email: Yup.string()
-                .email('El email no es válido')
-                .required('El email es obligatorio'),
+                        .email('El email no es válido')
+                        .required('El email es obligatorio'),
             password: Yup.string()
-                .required('El password no puede ir vacio')
-                .min(6, 'El password debe ser de al menos 6 caracteres')
+                        .required('El password no puede ir vacio')
+                        .min(6, 'El password debe ser de al menos 6 caracteres')
         }),
         onSubmit: async valores => {
-            console.log('enviando', valores);
-
-            const {nombre, apellido,email,password} = valores
+            // console.log('enviando');
+            // console.log(valores);
+            const { nombre, apellido, email, password } = valores
+            
 
             try {
                 const { data } = await nuevoUsuario({
-                                    variables: {
-                                        input: {nombre, apellido,email,password}
-                                    }
-                                })
-                // console.log(data);
+                    variables : {
+                        input: {
+                            nombre,
+                            apellido,
+                            email,
+                            password
+                        }
+                    }
+                });
+                console.log(data);
 
                 // Usuario creado correctamente
+                guardarMensaje(`Se creo correctamente el Usuario: ${data.nuevoUsuario.nombre} `);
 
-                // Redirigir a Login
+                setTimeout(() => {
+                    guardarMensaje(null);
+                    router.push('/login')
+                }, 3000);
+
+                // Redirigir usuario para iniciar sesión
+
             } catch (error) {
-                console.error(error);
+                guardarMensaje(error.message.replace('GraphQL error: ', ''));
+
+                setTimeout(() => {
+                    guardarMensaje(null);
+                }, 3000);
             }
         }
-    })
+    });
 
-    return (
+
+    // if(loading) return 'Cargando...';
+
+    const mostrarMensaje = () => {
+        return(
+            <div className="bg-white py-2 px-3 w-full my-3 max-w-sm text-center mx-auto">
+                <p>{mensaje}</p>
+            </div>
+        )
+    }
+
+    return ( 
+
         <>
-            <Layout>
-                <h1 className="text-center text-white text-2xl text-white font-light">NuevaCuenta</h1>
+            <Layout> 
+                {mensaje && mostrarMensaje() }
+
+                <h1 className="text-center text-2xl text-white font-light">Crear Nueva Cuenta</h1>
 
                 <div className="flex justify-center mt-5">
                     <div className="w-full max-w-sm">
-                        <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
-                            onSubmit={formik.handleSubmit}>
+                        <form
+                            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+                            onSubmit={formik.handleSubmit}
+                        >
+
                             <div className="mb-4">
                                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
                                     Nombre
-                            </label>
+                                </label>
+
                                 <input
                                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     id="nombre"
@@ -87,16 +130,19 @@ export default function NuevaCuenta() {
                                     onBlur={formik.handleBlur}
                                 />
                             </div>
-                            {formik.touched.nombre && formik.errors.nombre ? (
+
+                            { formik.touched.nombre && formik.errors.nombre ? (
                                 <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4" >
                                     <p className="font-bold">Error</p>
                                     <p>{formik.errors.nombre}</p>
                                 </div>
-                            ) : null}
+                            ) : null  }
+
                             <div className="mb-4">
                                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellido">
                                     Apellido
-                            </label>
+                                </label>
+
                                 <input
                                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     id="apellido"
@@ -104,67 +150,72 @@ export default function NuevaCuenta() {
                                     placeholder="Apellido Usuario"
                                     value={formik.values.apellido}
                                     onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
                                 />
                             </div>
-                            {formik.touched.apellido && formik.errors.apellido ? (
+
+                            { formik.touched.apellido && formik.errors.apellido ? (
                                 <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4" >
                                     <p className="font-bold">Error</p>
                                     <p>{formik.errors.apellido}</p>
                                 </div>
-                            ) : null}
+                            ) : null  }
+
                             <div className="mb-4">
                                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                                     Email
-                            </label>
+                                </label>
+
                                 <input
                                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     id="email"
                                     type="email"
-                                    autoComplete="new-email"
                                     placeholder="Email Usuario"
                                     value={formik.values.email}
                                     onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
                                 />
                             </div>
-                            {formik.touched.email && formik.errors.email ? (
+
+                            { formik.touched.email && formik.errors.email ? (
                                 <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4" >
                                     <p className="font-bold">Error</p>
                                     <p>{formik.errors.email}</p>
                                 </div>
-                            ) : null}
+                            ) : null  }
+
                             <div className="mb-4">
                                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                                     Password
-                            </label>
+                                </label>
+
                                 <input
                                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     id="password"
                                     type="password"
-                                    autoComplete="new-password"
                                     placeholder="Password Usuario"
                                     value={formik.values.password}
                                     onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
                                 />
                             </div>
-                            {formik.touched.password && formik.errors.password ? (
+
+                            { formik.touched.password && formik.errors.password ? (
                                 <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4" >
                                     <p className="font-bold">Error</p>
                                     <p>{formik.errors.password}</p>
                                 </div>
-                            ) : null}
+                            ) : null  }
+
                             <input
                                 type="submit"
                                 className="bg-gray-800 w-full mt-5 p-2 text-white uppercas hover:cursor-pointer hover:bg-gray-900"
                                 value="Crear Cuenta"
                             />
+
                         </form>
                     </div>
                 </div>
-
             </Layout>
         </>
-    )
+     );
 }
+ 
+export default NuevaCuenta;
